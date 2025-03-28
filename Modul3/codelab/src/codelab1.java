eeb6087491fdd7c38905d290c4db143c993b55c3
stealouve <grineebelajar@gmail.com>
2025-03-28 11:28:09 +0700

    Update

--- Modul3/codelab/src/codelab1.java
+++ Modul3/codelab/src/codelab1.java
@@ -35,7 +35,7 @@ class Hero extends Karakter {
 
     @Override
     public void serang(Karakter target, int damage, String jenisSerangan) {
-        System.out.println(getNama() + " melakukan serangan heroik: " + jenisSerangan + "!");
+        System.out.println(getNama() + " melakukan serangan heroik " + jenisSerangan + "!");
         super.serang(target, damage, jenisSerangan);
     }
 }
@@ -56,8 +56,8 @@ public class codelab1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Hero Kalea = new Hero("Kalea", 5000);
-        Monster Gloo = new Monster("Gloo", 6000);
+        Hero Kalea = new Hero("Kalea", 1000);
+        Monster Gloo = new Monster("Gloo", 1500);
 
         System.out.println("Pertarungan dimulai!\n");
         System.out.println(Kalea.getNama() + " vs " + Gloo.getNama());
@@ -67,19 +67,19 @@ public class codelab1 {
         while (Kalea.getKesehatan() > 0 && Gloo.getKesehatan() > 0) {
             System.out.println("Ronde " + ronde);
             System.out.println("Pilihan serangan " + Kalea.getNama() + ":");
-            System.out.println("1. Slash (500 damage)");
-            System.out.println("2. Power Strike (1000 damage)");
-            System.out.println("3. Ultimate Slash (2000 damage)");
+            System.out.println("1. WaveBreaker (250 damage)");
+            System.out.println("2. Tidal Strike (500 damage)");
+            System.out.println("3. Tsunami Slam (750 damage)");
             System.out.println("4. Cek Kesehatan");
             System.out.print("Pilih serangan: ");
             int pilihan = scanner.nextInt();
 
             if (pilihan == 1) {
-                Kalea.serang(Gloo, 500, "Slash");
+                Kalea.serang(Gloo, 250, "WaveBreaker");
             } else if (pilihan == 2) {
-                Kalea.serang(Gloo, 1000, "Power Strike");
+                Kalea.serang(Gloo, 500, "Tidal Strike");
             } else if (pilihan == 3) {
-                Kalea.serang(Gloo, 2000, "Ultimate Slash");
+                Kalea.serang(Gloo, 750, "Tsunami Slam");
             } else if (pilihan == 4) {
                 System.out.println(Kalea.getNama() + " memiliki kesehatan: " + Kalea.getKesehatan());
                 System.out.println(Gloo.getNama() + " memiliki kesehatan: " + Gloo.getKesehatan());
@@ -91,7 +91,7 @@ public class codelab1 {
 
             if (Gloo.getKesehatan() > 0) {
                 System.out.println(Gloo.getNama() + " menyerang balik!");
-                Gloo.serang(Kalea, 750, "Fire Breath");
+                Gloo.serang(Kalea, 200, "Slam, Slam");
             }
 
             ronde++;
