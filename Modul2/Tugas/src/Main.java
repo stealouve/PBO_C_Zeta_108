cc0117f2ea5769c8b26db7d64ba7d29a8b48f7f2
stealouve <grineebelajar@gmail.com>
2025-03-28 11:18:59 +0700

    Update

--- Modul2/Tugas/src/Main.java
+++ Modul2/Tugas/src/Main.java
@@ -1,15 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class KarakterGame {
+    private String nama;
+    private int kesehatan;
+
+    public KarakterGame(String nama, int kesehatan) {
+        this.nama = nama;
+        this.kesehatan = kesehatan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getKesehatan() {
+        return kesehatan;
+    }
+
+    public void setKesehatan(int kesehatan) {
+        this.kesehatan = kesehatan;
+    }
+
+    public void serang(KarakterGame target) {
+    } //superclass
+}
+
+class Pahlawan extends KarakterGame {
+
+    public Pahlawan(String nama, int kesehatan) {
+        super(nama, kesehatan);
+    }
+
+    @Override
+    public void serang(KarakterGame target) {
+        System.out.println(getNama() + " menyerang " + target.getNama() + " menggunakan Lantern Flare!");
+        target.setKesehatan(target.getKesehatan() - 40);
+        System.out.println("Kesehatan " + target.getNama() + " sekarang: " + target.getKesehatan());
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Pahlawan pahlawan = new Pahlawan("Zhuxin", 150);
+        KarakterGame musuh = new KarakterGame("Xavier", 200);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        pahlawan.serang(musuh);
     }
 }
